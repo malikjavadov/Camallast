4fabfc80bf7af8aec0cc9a5bb88e49f2468bf5ad
malikjavadov <javadov49@gmail.com>
2020-05-01 21:18:28 +0200

    the last

---
+++ app/src/main/java/com/example/camal/NeurologistsRecyclerview_main.java
@@ -0,0 +1,14 @@
+package com.example.camal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NeurologistsRecyclerview_main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_neurologists_recyclerview_main);
+    }
+}
